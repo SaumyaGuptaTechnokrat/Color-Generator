73a89e68e39c6d47b96dfb7be2f446dd5fc88f02
SaumyaGuptaTechnokrat <65768914+SaumyaGuptaTechnokrat@users.noreply.github.com>
2023-08-10 17:42:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,33 +6,6 @@ var typed = new Typed('#text', {
 });
 // var colorChanged = document.getElementById("colorChanged");
 
-//Hashcodes
-function getRandomHexColor() {
-    const ch = document.getElementById('box').value;
-    return ch.toString(16);
-  }
-
-  function changeColor() {
-    const colorChanged = document.getElementById('colorChanged');
-    const button = document.getElementById('colorChanger');
-
-    const randomColor = getRandomHexColor(); // Generate a random color
-
-    // colorChanged.style.backgroundColor = randomColor;
-
-    const r = parseInt(randomColor.substr(1, 2), 16);
-    const g = parseInt(randomColor.substr(3, 2), 16);
-    const b = parseInt(randomColor.substr(5, 2), 16);
-    const perceivedLightness = (r * 299 + g * 587 + b * 114) / 1000;
-
-    const textColor = perceivedLightness > 128 ? '#000' : '#fff';
-    button.style.color = textColor;
-    button.style.backgroundColor = randomColor;
-  }
-
-  const colorChanger = document.getElementById('colorChanger');
-  colorChanger.addEventListener('mouseover', changeColor);
-//Hashcode
 
 
 function colorGenerator() {
@@ -49,16 +22,9 @@ function colorGenerator() {
 
     // Take the hex code
     document.getElementById('box').value = color;
-    // if(colorChanged.style.backgroundColor=="#ffffff"){
-//     colorChanged.style.color="black";
-//     console.log("Color White");
-// }
-// else if(colorChanged .style.backgroundColor=="black"){
-//     colorChanged.style.color = "white";
-// }
+
 }
-// When user clicks over color picker,
-// myColor() function is called
+
 document.getElementById('colorPicker')
     .addEventListener('input', colorGenerator);
     function colorcode(){
@@ -67,6 +33,39 @@ document.getElementById('colorPicker')
         document.getElementById('colorHexCode').innerText = colorCode;
 
         document.getElementById("colorChanged").style.backgroundColor = colorCode;
-  
- 
+    
+      
+        if(colorChanged.style.backgroundColor=="white"){
+            colorChanged.style.color="black";
+        }
+        else if(colorChanged .style.backgroundColor=="black"){
+            colorChanged.style.color = "white";
+        }
     }
+   
+   
+    function getRandomHexColor() {
+        const ch = document.getElementById('box').value;
+        return ch.toString(16);
+      }
+    
+      function changeColor() {
+        const colorChanged = document.getElementById('colorChanged');
+        const button = document.getElementById('colorChanger');
+    
+        const randomColor = getRandomHexColor(); // Generate a random color
+    
+        // colorChanged.style.backgroundColor = randomColor;
+    
+        const r = parseInt(randomColor.substr(1, 2), 16);
+        const g = parseInt(randomColor.substr(3, 2), 16);
+        const b = parseInt(randomColor.substr(5, 2), 16);
+        const perceivedLightness = (r * 299 + g * 587 + b * 114) / 1000;
+    
+        const textColor = perceivedLightness > 128 ? '#000' : '#fff';
+        button.style.color = textColor;
+        button.style.backgroundColor = randomColor;
+      }
+    
+      const colorChanger = document.getElementById('colorChanger');
+      colorChanger.addEventListener('mouseover', changeColor);
